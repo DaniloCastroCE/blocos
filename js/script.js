105454ed2aeac56a09f3196d7ab2917e464c9ce6
DaniloCastroCE <38533079+DaniloCastroCE@users.noreply.github.com>
2025-01-10 04:08:10 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const func_acessar = (op) => {
+    switch (op) {
+        case 1:
+            window.location.assign('https://bloco-qz7l.onrender.com/')
+            break;
+        case 2:
+            window.location.assign('https://danilocastroce.github.io/bloco-script-v2/')
+            break;
+    
+        default:
+            break;
+    }
+}
